c621657a781814e3f1f710b1571529cf3e938754
chaitu-chaitu <natukulachaithanyakumar@gmail.com>
2026-03-02 21:53:46 +0530

    removed jinja2

--- app/main.py
+++ app/main.py
@@ -1,21 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from app.models.schemas import ChatRequest, ChatResponse
 from app.services.service import generate_response
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi import Request
 
 app = FastAPI(title="Local Mind")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/")
-def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
-
-
+def home():
+    return FileResponse("templates/index.html")
 
 @app.post("/chat", response_model=ChatResponse)
 def chat(request: ChatRequest):
